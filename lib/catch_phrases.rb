1387f0a3f6296ca946485e053a9259cceedf3bef
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-07 00:40:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,14 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  says = "It's Dangerous To Go Alone! Take This."
+  puts says
+end
+
+def all_phrases
+  mario 
+  toadstool 
+  link 
+end 
